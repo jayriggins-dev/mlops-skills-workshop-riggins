8bbc003489345e24dde40ce72b5032d91877ddaf
jayriggins-dev <jay.riggins@siriusfederal.com>
2025-08-28 06:19:59 -0400

    Delete Challenge3/create_model.sql

--- Challenge3/create_model.sql
+++
@@ -1,3 +0,0 @@
-CREATE OR REPLACE MODEL `qwiklabs-gcp-04-db5d757ff012.Lab3B.gemini_weather_model`
-REMOTE WITH CONNECTION `projects/qwiklabs-gcp-04-db5d757ff012/locations/us/connections/riggins_vertex_connection`
-OPTIONS (endpoint = 'gemini-2.0-flash')
